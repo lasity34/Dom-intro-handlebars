61ba8167a8f4eadf78e299e487cee4c43eb78bd5
Bjorn Worrall <85297118+lasity34@users.noreply.github.com>
2023-05-08 14:14:40 +0200

    handles bars setup

---
+++ .history/js/text-bill_20230508114458.js
@@ -0,0 +1,76 @@
+// get a reference to the textbox where the bill type is to be entered
+
+//get a reference to the add button
+
+//create a variable that will keep track of the total bill
+
+//add an event listener for when the add button is pressed
+
+//in the event listener check if the value in the bill type textbox is 'sms' or 'call'
+// * add the appropriate value to the running total
+// * add nothing for invalid values that is not 'call' or 'sms'.
+// * display the latest total on the screen\index.html
+
+const billTypeText = document.querySelector(".billTypeText");
+const textTotalAddBtn = document.querySelector(".addToBillBtn");
+const textTotalRemoveBtn = document.querySelector(".resetBtn");
+const totalElem = document.querySelector(".total");
+const textCostDataElem = document.querySelector("#dropDown");
+
+
+let callsTotal = 0.0;
+let smsTotal = 0.0;
+let totaltext = 0.0;
+
+function textBillTotal() {
+  const billTypeEntered = billTypeText.value.trim();
+
+  if (billTypeEntered === "call") {
+    callsTotal += 2.75;
+  } else if (billTypeEntered === "sms") {
+    smsTotal += 0.75;
+  }
+
+  totaltext = callsTotal + smsTotal;
+  updateTextTemplate();
+
+}
+function removeTotal() {
+  callsTotal = 0;
+  smsTotal = 0;
+  totaltext = 0
+  updateTextTemplate();
+}
+
+function updateTextTemplate() {
+  const templateSource = document.querySelector("#billTemplate").innerHTML;
+  const textTemplate = Handlebars.compile(templateSource);
+
+  const costData = {
+    differentCosts: [
+      { name: "Call", total: callsTotal.toFixed(2), extraClass : "" },
+      {
+        name: "Sms",
+        total: smsTotal.toFixed(2),
+        extraClass : "" 
+      },
+      {
+        name: "",
+        total: totaltext.toFixed(2),
+        extraClass : totaltext > 50 ? "danger" : totaltext > 30 ? "warning" : ""
+      },
+    ],
+   
+  };
+
+  const userDataHTML = textTemplate(costData);
+
+  textCostDataElem.innerHTML = userDataHTML;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  updateTextTemplate();
+});
+
+textTotalAddBtn.addEventListener("click", textBillTotal);
+textTotalRemoveBtn.addEventListener("click", removeTotal);
---
+++ js/radio-bill.js
@@ -0,0 +1,77 @@
+// get a reference to the sms or call radio buttons
+
+//get a reference to the add button
+
+//create a variable that will keep track of the total bill
+
+//add an event listener for when the add button is pressed
+
+//in the event listener get the value from the billItemTypeRadio radio buttons
+// * add the appropriate value to the running total
+// * add nothing for invalid values that is not 'call' or 'sms'.
+// * display the latest total on the screen
+
+const radioBillAddBtnElement = document.querySelector(".radioBillAddBtn");
+
+// "input[name='billItemType']:checked"
+const callTotalTwoElement = document.querySelector(".callTotalTwo");
+const smsTotalTwoElement = document.querySelector(".smsTotalTwo");
+const totalTwoElement = document.querySelector(".totalTwo");
+
+let smsTotalRadio = 0;
+let callTotalRadio = 0;
+let radioTotal = 0;
+function calculateRadioBtn() {
+  const checkedRadioBtnElement = document.querySelector(
+    "input[name='billItemType']:checked"
+  );
+
+  if (checkedRadioBtnElement) {
+    const billItemType = checkedRadioBtnElement.value;
+
+    if (billItemType === "call") {
+      callTotalRadio += 2.75;
+    } else if (billItemType === "sms") {
+      smsTotalRadio += 0.75;
+    }
+  }
+
+  radioTotal = callTotalRadio + smsTotalRadio;
+
+  updateRadioTemplate();
+
+  const radioItem = document.querySelectorAll(".radioItem");
+
+
+}
+
+function updateRadioTemplate(type) {
+  const templateSource = document.querySelector("#billTemplate").innerHTML;
+  const userTemplate = Handlebars.compile(templateSource);
+  const radioCostDataElem = document.querySelector(".radioTotals");
+  const costData = {
+    differentCosts: [
+      { name: "Call", total: callTotalRadio.toFixed(2), extraClass : "" },
+      {
+        name: "Sms",
+        total: smsTotalRadio.toFixed(2),
+        extraClass : ""
+      },
+      {
+        name: "",
+        total: radioTotal.toFixed(2),
+        extraClass : radioTotal > 50 ? "danger" : radioTotal > 30 ? "warning" : ''
+      },
+    ],
+   
+  };
+
+  const userDataHTML = userTemplate(costData);
+  radioCostDataElem.innerHTML = userDataHTML;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  updateRadioTemplate("radio");
+});
+
+radioBillAddBtnElement.addEventListener("click", calculateRadioBtn);
